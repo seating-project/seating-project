33bd2dcdc21125679bef32863a1eef5fc98b944b
Roshan Jagadish <roshan10.rj@gmail.com>
2022-09-28 19:25:36 +0530

    CSVs and MySQL Code

--- seating.py
+++ seating.py
@@ -1,5 +1,26 @@
-nckt_array = [i for i in range(1,51)] # Array for circuit group students
-ckt_array = [i for i in range(51,101)] # Array for non circuit group students
+import mysql.connector
+db = mysql.connector.connect(host="localhost", user="root", passwd="root")
+
+nckt_array = [] # Array for circuit group students
+ckt_array = [] # Array for non circuit group students
+sample1 = []
+sample2 = []
+
+cursor = db.cursor()
+
+cursor.execute("USE secondthird")
+cursor.execute("SELECT RegisterNo FROM cseii where Gender='M'")
+cseData = cursor.fetchall()
+sample1.extend(cseData)
+cursor.execute("SELECT RegisterNo FROM mechii where Gender='M'")
+mechData = cursor.fetchall()
+sample2.extend(mechData)
+
+for i in sample1:
+    ckt_array.append(i[0])
+for i in sample2:
+    nckt_array.append(i[0])
+
 class_array = ["F1","F2","F3"] #Room names
 curr_room = list()
 room_dict = dict()
@@ -18,11 +39,15 @@ try:
             c += 1
         room_dict[i] = curr_room
         curr_room = []
-except(IndexError):
+
+except (IndexError):
+    
     room_dict[i] = curr_room
     curr_room = []
     print("Seats alloted!")
     print(room_dict)
+
+print(room_dict)
 
 # {"F1":[[2, 52], [3, 53]],"F2":[[2, 52], [3, 53]]}
 
