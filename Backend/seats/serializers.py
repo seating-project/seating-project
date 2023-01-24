2c07bcb316ff7d8a17a40b1bfdcadfb6a6ceeb5f
Roshan Jagadish <roshan10.rj@gmail.com>
2023-01-24 19:47:28 +0530

    Requirements added

--- Backend/seats/serializers.py
+++ Backend/seats/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Cseii, Mechii
 
+
 class CseiiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cseii
         fields = '__all__'
+
 
 class MechiiSerializer(serializers.ModelSerializer):
     class Meta:
