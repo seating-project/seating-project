d4f69a7bb7f86d9d25ed48cb9d0f97114a6ad37b
Sai Sabarish <saisabarishmail@gmail.com>
2023-02-01 16:18:21 +0530

    Created Pages for forms ✅

---
+++ frontend/app/examform/page.tsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+import ExamForm from "../../components/ExamForm";
+
+export default function Form() {
+  return <ExamForm />;
+}
---
+++ frontend/app/templateform/page.tsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+import TemplateForm from "../../components/TemplateForm";
+
+export default function Form() {
+  return <TemplateForm />;
+}
