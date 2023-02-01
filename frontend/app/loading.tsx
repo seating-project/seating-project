e8e56b3c74b1ca0a74fc8ef8ac29adf5ec738652
Roshan Jagadish <roshan10.rj@gmail.com>
2023-02-01 15:51:48 +0530

    Loading screen reuploaded

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -3,26 +3,27 @@
 import { useEffect } from "react";
 
 const LoadingPage = () => {
-  useEffect(() => {
-    const loadingElem = document.getElementById("loading");
-    const loadingTextElem = document.getElementById("loading-text");
-    let loading = 0;
+  // useEffect(() => {
+  //   const loadingElem = document.getElementById("loading");
+  //   const loadingTextElem = document.getElementById("loading-text");
+  //   let loading = 0;
 
-    const loadingInterval = setInterval(() => {
-      loading += 10;
-      loadingElem.style.width = `${loading}%`;
-      loadingTextElem.innerText = `Loading... ${loading}%`;
-    }, 500);
+  //   const loadingInterval = setInterval(() => {
+  //     loading += 10;
+  //     loadingElem.style.width = `${loading}%`;
+  //     loadingTextElem.innerText = `Loading... ${loading}%`;
+  //   }, 500);
 
-    return () => {
-      clearInterval(loadingInterval);
-    };
-  }, []);
+  //   return () => {
+  //     clearInterval(loadingInterval);
+  //   };
+  // }, []);
 
   return (
-    <div className="loading-container">
-      <div id="loading" className="loading-bar"></div>
-      <div id="loading-text" className="loading-text"></div>
+    <div className="bg-dark-blue flex justify-center items-center h-screen">
+      <div className="w-12 h-12 border-8 border-t-8 border-sky-blue rounded-full text-white animate-spin">
+        <svg viewBox="0 0 24 24"></svg>
+      </div>
     </div>
   );
 };
