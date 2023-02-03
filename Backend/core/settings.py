270aca0c29eee9560a5ed211a8279ed9f57e13b3
Sai Sabarish <saisabarishmail@gmail.com>
2023-02-03 11:52:21 +0530

    Working on components 🔨

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from .creds import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b9=rc9qv7t(!=o2cy**@l*g+lnc-xqi35e&(3o2=_z+o%yf5r5"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
